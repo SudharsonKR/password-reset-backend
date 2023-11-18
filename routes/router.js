9aae4b30607be77aa1f3f800f5212aff5795fd62
SudharsonKR <sudharsonkr@gmail.com>
2023-11-18 07:18:25 +0530

    second Commit

--- routes/router.js
+++ routes/router.js
@@ -130,7 +130,7 @@ router.post("/sendpasswordlink",async(req,res)=>{
 
         // token generate for reset password
         const token = jwt.sign({_id:userfind._id},keysecret,{
-            expiresIn:"1d" // 1day
+            expiresIn:"1d" // 
         });
         
         const setusertoken = await userdb.findByIdAndUpdate({_id:userfind._id},{verifytoken:token},{new:true});
@@ -140,7 +140,7 @@ router.post("/sendpasswordlink",async(req,res)=>{
                 from:process.env.MAIL_ID,
                 to:email,
                 subject:"Sending Email For password Reset",
-                text:`This Link expired after 3 minutes http://localhost:3000/forgot-password/${userfind.id}/${setusertoken.verifytoken}`
+                text:`This Link expired after 3 minutes https://dancing-cupcake-3757ce.netlify.app/forgot-password/${userfind.id}/${setusertoken.verifytoken}`
             }
 
             transporter.sendMail(mailOptions,(error,info)=>{
